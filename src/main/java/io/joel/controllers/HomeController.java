0069049b114f99f6de96a17aa2655f32a6a08798
taddeimania <jtaddei@gmail.com>
2017-07-03 09:13:50 -0400

    Initial commit

---
+++ src/main/java/io/joel/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package io.joel.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    String index() {
+        return "index";
+    }
+
+    @GetMapping("/greeting")
+    String greeting(@RequestParam(value="name", required=false, defaultValue="Joel") String name, Model model ) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+}
